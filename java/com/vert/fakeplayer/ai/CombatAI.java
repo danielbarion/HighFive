63aa45337f9cca9e37964f5b01a071fa607100bd
Daniel Barion <danielbarionn@gmail.com>
2019-05-18 11:54:02 -0300

    Undo last commit and fix correctly the reason of exception

--- java/com/vert/fakeplayer/ai/CombatAI.java
+++ java/com/vert/fakeplayer/ai/CombatAI.java
@@ -168,13 +168,14 @@ public abstract class CombatAI extends FakePlayerAI {
     protected Skill getRandomAvaiableFighterSpellForTarget() {
         if (!_fakePlayer.isDead()) {
             List<OffensiveSpell> spellsOrdered = getOffensiveSpells().stream().sorted(Comparator.comparingInt(BotSkill::getPriority)).collect(Collectors.toList());
-            Skill skill = null;
 
             int skillListSize = spellsOrdered.size();
 
-            if (skillListSize > 0) {
-                skill = _fakePlayer.getKnownSkill(spellsOrdered.get(skillIndex).getSkillId());
+            if ((skillIndex - 1) == skillListSize) {
+                skillIndex = 0;
             }
+
+            Skill skill = _fakePlayer.getKnownSkill(spellsOrdered.get(skillIndex).getSkillId());
 
             boolean hasReuseHashCode = _fakePlayer.hasSkillReuse(skill.getReuseHashCode());
 
